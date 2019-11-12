c1de2583f6ec67d90c63fbc36b4eb8aab2997619
Dostaglou <dostaglou@gmail.com>
2019-11-12 18:27:18 +0900

    basic type creation for Sub and Man

---
+++ app/graphql/resovlers/base.rb
@@ -0,0 +1,15 @@
+module Resolvers
+    class Base < GraphQL::Schema::Resolver
+        # The methods below will check if there is a current user
+        # me? for raising errors
+        # me for getting the current_user object conveniently
+
+        def me?
+            raise GraphQL::ExecutionError.new("Must be signed in to access this feature") unless context[:current_user]
+        end
+
+        def me
+            context[:current_user]
+        end
+    end
+end
