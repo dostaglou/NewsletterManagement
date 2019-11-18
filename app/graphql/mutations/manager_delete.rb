df50037cb8e5f27f72fea68d76077e7cb6b5ae25
Dostaglou <dostaglou@gmail.com>
2019-11-18 12:49:51 +0900

    can now delete manager self

--- app/graphql/mutations/manager_delete.rb
+++ app/graphql/mutations/manager_delete.rb
@@ -1,13 +1,13 @@
 module Mutations
   class ManagerDelete < BaseMutation
     field :msg, String, null: false
-    field :self, Types::ManagerType, null: false
+    field :manager, Types::ManagerType, null: false
 
     def resolve
       me?
       me.destroy 
       message = "Your manager account and all its newsletters have been deleted"
-      {msg: message, self: me}
+      {msg: message, manager: me}
     end
   end
 end
