ec24d2c489e66536313d341fad17654d773029cd
Dostaglou <dostaglou@gmail.com>
2019-11-18 12:49:22 +0900

    delete_all to destroy to trigger other models destroy

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -2,7 +2,7 @@ class Manager < ApplicationRecord
     has_secure_password
     validates :name, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
-    has_many :newsletters, dependent: :delete_all
+    has_many :newsletters, dependent: :destroy
     has_many :subscribers, through: :newsletters
     has_many :templates, through: :newsletters
 end
